d234efbfe3a92e5437743a7bf1e270dc17b4f5cf
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-11-06 20:28:41 +0100

    chore: update nuxt.config.ts and tsconfig.json for runtime configuration and type definitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    CONTENT_SPACE_ID: process.env.CONTENT_SPACE_ID,
+    CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+    CONTENTFUL_DELIVERY_TOKEN: process.env.CONTENTFUL_DELIVERY_TOKEN,
+    CONTENTFUL_PREVIEW_TOKEN: process.env.CONTENTFUL_PREVIEW_TOKEN,
+  },
+});
