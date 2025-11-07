56dc6f027de968cfc46355f89793dd5d490966b0
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-11-07 22:45:50 +0100

    chore: Adds ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+// Run this command to generate base config and vs code settings:
+// pnpm dlx @antfu/eslint-config@latest
+
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  },
+  {
+    rules: {
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["off"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          tsconfigRootDir: ".",
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+      "vue/block-order": ["error", {
+        order: ["script", "template", "style"],
+      }],
+    },
+  },
+  {
+    files: ["app/components/**/*.{vue,ts,tsx,js,jsx}"],
+    rules: {
+      "unicorn/filename-case": "off",
+    },
+  },
+);
