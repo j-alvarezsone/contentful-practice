ee547ae59759d3a38637a0ab51b6d89e0867c107
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-11-13 18:55:25 +0100

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ["./app/assets/css/main.css"],
   runtimeConfig: {
     CONTENT_SPACE_ID: process.env.NUXT_CONTENTFUL_SPACE_ID,
     CONTENTFUL_ENVIRONMENT: process.env.NUXT_CONTENTFUL_ENVIRONMENT,
