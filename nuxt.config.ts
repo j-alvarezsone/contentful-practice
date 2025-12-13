daf7d9e5a8c21afaa8d35dc531357a9500565429
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-12-13 16:50:37 +0000

    chore: add @nuxtjs/supabase module and configure in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          "Instrument+Sans": true,
-          "Geist": ["100..900"],
-        },
-        display: "swap",
+  modules: [[
+    "@nuxtjs/google-fonts",
+    {
+      families: {
+        "Instrument+Sans": true,
+        "Geist": ["100..900"],
       },
-    ],
-  ],
+      display: "swap",
+    },
+  ], "@nuxtjs/supabase"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   vite: {
@@ -30,5 +28,9 @@ export default defineNuxtConfig({
   },
   typescript: {
     typeCheck: true,
+  },
+  supabase: {
+    url: process.env.NUXT_SUPABASE_URL,
+    key: process.env.NUXT_SUPABASE_PUBLISHABLE_KEY,
   },
 });
